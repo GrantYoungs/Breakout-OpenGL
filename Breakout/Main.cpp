f2ddc10fed9a1cf195d04afe74b0024b164e5d64
FreakingGrant <youngsgr@msu.edu>
2020-04-28 19:31:49 -0400

    Created vertex shader object and ensured successful compilation

--- Breakout/Main.cpp
+++ Breakout/Main.cpp
@@ -11,6 +11,14 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+// Source code for a vertex shader in the OpenGL Shading Language, written as a C string
+const char* vertexShaderSource = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
 int main()
 {
     // Initilize GLFW, GLAD, and OpenGL
@@ -39,6 +47,36 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
+
+    // Build and compile our shader programs
+    // -------------------------------------
+    
+    // Vertex shader
+    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL); // Attach that C string of vertex shader source code
+    glCompileShader(vertexShader);  // Compile that source code
+    // Check for vertex shader compile time errors
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+
+    // Set up vertex data (and buffer(s)) and configure vertex attributes
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    unsigned int VBO;   // Vertex Buffer Object (array pointer)
+    glGenBuffers(1, &VBO);  // Generates a Buffer Object
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);     // Bind the newly created Vertex Buffer to the GL_ARRAY_BUFFER target
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);  // Copy the previously defined vertex data into the buffer's memory
 
     /*
     * Render loop - this will keep the window running until it has been explicitly told to close
